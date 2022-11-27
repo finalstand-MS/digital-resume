0e76d4424401247b190e389f4e6bc6f610b7f68c
finalstand-MS <81987612+finalstand-MS@users.noreply.github.com>
2022-11-27 15:10:59 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit  as st
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+# ---- HEADER SECTION ----
+
+
+
+st.subheader("Hoi, ik ben Merijn :wave:.")
+st.title("Een website om mijzelf te verkopen aan bedrijven")
+st.write("Ik ben een gepassioneerde wereldverbeteraar.")
+st.write("[Blog >](mschakelaar.blogspot.com)")
